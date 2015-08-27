fbb1fee6fb2209cbc11683e28ff105b5a474ff7c
filz <filz@Filz-Linux-PC>
2015-08-27 14:56:34 +0100

    Bug fixed on add new Application

--- BMAuth/src/main/wro/js/applications/edit.js
+++ BMAuth/src/main/wro/js/applications/edit.js
@@ -5,4 +5,7 @@ angular.module('bmauth.applications', [])
 .controller('ApplicationsCtrl', ['$scope', function($scope) {
 	
 	console.debug("in applications controller");
+	
+	$scope.application = {"active": true, "testMode": "0"};
+	
 }]);
